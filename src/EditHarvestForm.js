b5d8e45adb224a034524c1925a8e21c9f97bfbcf
Sasha Hernandez <270023564@cit3-102m-17.acg.ac.nz>
2019-08-21 15:45:05 +1200

    Set up react

---
+++ src/EditHarvestForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class EditHarvestForm extends Component {
+    constructor(props){
+      super(props);
+    }
+
+    render(){
+      return(
+       <form>
+         {/* insert form here */}
+       </form>
+      )
+    }
+}
+
+export default EditHarvestForm;
